6792cb5e9e2d894415b3f0df4a50b747279caa32
Igor Iric <igor.iric@plainconcepts.com>
2024-08-25 16:15:18 +0200

    Taking Client ID from the Config

--- azure-appservice/Program.cs
+++ azure-appservice/Program.cs
@@ -9,11 +9,14 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
+// Retrieve the ClientId from configuration
+var clientId = builder.Configuration["AzureAd:ClientId"];
+
 // Configure Azure AD Authentication
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
-// Configure Swagger with OAuth2
+// Configure Swagger with OAuth2 and dynamic scope
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
@@ -22,7 +25,6 @@ builder.Services.AddSwaggerGen(c =>
         Version = "v1"
     });
 
-    // Configure Swagger to use OAuth2
     c.AddSecurityDefinition("oauth2", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
     {
         Type = Microsoft.OpenApi.Models.SecuritySchemeType.OAuth2,
@@ -34,7 +36,7 @@ builder.Services.AddSwaggerGen(c =>
                 TokenUrl = new Uri($"https://login.microsoftonline.com/{builder.Configuration["AzureAd:TenantId"]}/oauth2/v2.0/token"),
                 Scopes = new Dictionary<string, string>
                 {
-                    { "api://YourClientId/.default", "Access the API" }
+                    { $"api://{clientId}/.default", "Access the API" }
                 }
             }
         }
@@ -51,7 +53,7 @@ builder.Services.AddSwaggerGen(c =>
                     Id = "oauth2"
                 }
             },
-            new[] { "api://YourClientId/.default" }
+            new[] { $"api://{clientId}/.default" }
         }
     });
 });
@@ -65,8 +67,8 @@ if (app.Environment.IsDevelopment())
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
         c.RoutePrefix = string.Empty;
-        c.OAuthClientId(builder.Configuration["AzureAd:ClientId"]);
-        c.OAuthUsePkce(); // Use PKCE for security
+        c.OAuthClientId(clientId);  // Use ClientId from configuration
+        c.OAuthUsePkce();
     });
 }
 
