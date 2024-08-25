5dcf07467d5b4b3f4aaecea8bb9b93aa5be6f7de
Igor Iric <iric_i@hotmail.com>
2024-08-25 14:25:53 +0200

    Adding Function Files

---
+++ azure-function/ProcessServiceBusMessage.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Azure_Infra___Azure_Function
+{
+    public class ProcessServiceBusMessage
+    {
+        [FunctionName("ProcessServiceBusMessage")]
+        public void Run([ServiceBusTrigger("mytopic", "mysubscription", Connection = "")]string mySbMsg, ILogger log)
+        {
+            log.LogInformation($"C# ServiceBus topic trigger function processed message: {mySbMsg}");
+        }
+    }
+}
